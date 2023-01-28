b81f72f0678265fd6eabf5c41b0d08f2fa740ea7
Mariam1519 <121454566+maiam1519@users.noreply.github.com>
2023-01-28 19:45:24 +0000

    ClawOpen/Close

---
+++ src/main/java/frc/robot/commands/Claw.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+
+public class Claw {
+
+    public static Claw getInstance() {
+        return null;
+    }
+
+    public void stop() {
+    }
+
+    public void ClawClose() {
+    }
+
+    public void ClawOpen() {
+    }
+
+}
