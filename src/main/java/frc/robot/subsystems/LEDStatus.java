0d603ebdc38948a2f6321ab72645dc91a0ba247d
Jelver Bautista <120145032+Jelverr@users.noreply.github.com>
2023-02-03 23:01:30 +0000

    Added methods LEDStatus.java

--- src/main/java/frc/robot/subsystems/LEDStatus.java
+++ src/main/java/frc/robot/subsystems/LEDStatus.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -8,16 +12,41 @@ import frc.robot.Ports;
 import frc.robot.utils.LEDColors.Colors;
 
 public class LEDStatus extends SubsystemBase {
-    private static Spark led = new Spark(Ports.BLINKIN_PORT);
+    private final Spark led;
+    private final NetworkTableInstance netTable;
+    private final NetworkTable FMSTable;
+    private final DoubleSubscriber FMSControlData;
+    private final BooleanSubscriber IsRedAlliance;
 
-    //Colors indicating the pick up sequence.
-    public static final Colors READY_PICKUP = Colors.DARK_GREEN;
-    public static final Colors PICKUP = Colors.GREEN;
-    public static final Colors DONE_PICKUP = Colors.LAWN_GREEN;
+    private LEDStatus() {
+        led = new Spark(Ports.BLINKIN_PORT);
+        netTable = NetworkTableInstance.getDefault();
+        FMSTable = netTable.getTable("FMSInfo");
+        FMSControlData = FMSTable.getDoubleTopic("FMSControlData").subscribe(0);
+        IsRedAlliance = FMSTable.getBooleanTopic("IsRedAlliance").subscribe(false);
+    }
 
-    //These colors will change
-    public static final Colors ALLIANCE_BLUE = Colors.BLUE;
-    public static final Colors ALLIANCE_RED = Colors.RED;
 
+
+    
+    public void setLed(Colors color) {
+        led.set(color.getColor());
+    }
+
+
+    public void RobotStatus(){
+        if(FMSControlData.get() == 33){
+            setLed(Colors.STROBE_GOLD);
+        }
+        else{
+
+            if(IsRedAlliance.get()){
+                setLed(Colors.STROBE_RED);
+            }
+            else{
+                setLed(Colors.STROBE_BLUE);
+            }
+        }
+    }    
 
 }
