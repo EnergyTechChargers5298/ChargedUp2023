76740f8d93d1f28e4454fe961021ea6de25effeb
Aiden Sing <98428413+Catears1@users.noreply.github.com>
2024-03-02 17:13:05 -0500

    Add files via upload

---
+++ src/main/java/frc/robot/utils/RetroCam.java
@@ -0,0 +1,51 @@
+package frc.robot.utils;
+
+import java.util.List;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.TargetCorner;
+
+import edu.wpi.first.math.geometry.Transform3d;
+
+public class RetroCam {
+
+    private PhotonCamera camera;
+    private PhotonPipelineResult result;
+    private PhotonTrackedTarget bestTarget;
+
+    public RetroCam(String cameraName) {
+        camera = new PhotonCamera(cameraName);
+        update();
+    }
+
+    public void update() {
+        this.result = camera.getLatestResult();
+        this.bestTarget = result.getBestTarget();
+    }
+
+    public double getYaw() {
+        return bestTarget.getYaw();
+    }
+
+    public double getPitch() {
+        return bestTarget.getPitch();
+    }
+
+    public double getArea() {
+        return bestTarget.getArea();
+    }
+
+    public double getSkew() {
+        return bestTarget.getSkew();
+    }
+
+    public List<TargetCorner> getCorners() {
+        return bestTarget.getDetectedCorners();
+    }
+
+    public Transform3d getCameraToTarget() {
+        return bestTarget.getBestCameraToTarget();
+    }
+}
